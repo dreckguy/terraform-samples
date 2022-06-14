945986d03fa7c198855a3a73ffbddc6b3605a1f8
ophir.h <ophir.h@quali.com>
2022-06-14 11:17:26 +0300

    update html

--- website/object.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.files.id
-  key    = "index.html"
-  source = "./index.html"
-  content_type = "text/html"
-  acl = "public-read"
-}
--- website/s3_bucket.tf
+++ website/s3_bucket.tf
@@ -1,7 +1,2 @@
 resource "aws_s3_bucket" "files" {
-  bucket = "ophirus-test-bucket"
-
-  tags = {
-    Name        = "Ophirus Test bucket"
-  }
 }
