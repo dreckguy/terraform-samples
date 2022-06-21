e74550c2fb120ca02d55268457cd8f2e2475b4a9
ophir.h <ophir.h@quali.com>
2022-06-21 15:27:37 +0300

    remove tags

--- eks-cluster/vpc.tf
+++ eks-cluster/vpc.tf
@@ -30,18 +30,4 @@ module "vpc" {
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
-
-  tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
 }
