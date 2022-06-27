2fec1837c7100753b3a9318e8aca43ee38060ee0
ophir.h <ophir.h@quali.com>
2022-06-27 19:24:31 +0300

    revert to official module

--- eks-cluster/eks-cluster.tf
+++ eks-cluster/eks-cluster.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source          = "https://github.com/dreckguy/terraform-aws-eks"
+  source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.22"
